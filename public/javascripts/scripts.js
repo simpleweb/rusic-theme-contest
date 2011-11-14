0978635937ec0f1db9a9e3c3b7240019deea0c54
Dale Williams <dale@simpleweb.co.uk>
2011-11-14 11:04:16 +0000

    small tweaks and IE fixes

---
+++ public/javascripts/scripts.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    
+    // Add alt margins to popular widget
+    $('.widget.popular ul li').filter(function(index){
+    return (index%2 == 1);
+    }).addClass('alt');
+
+    // check for countdown plugin
+    if($.fn.countdown) {
+        // grab clock element and extract "expires at" timestamp
+    	var $time = $("time#expires-at"),
+          timestamp = Date.parse($time.attr('datetime')),
+          expiresAt = new Date(timestamp);
+          $time.countdown({
+            until: expiresAt,
+            format: 'dHMS'
+          });
+          
+    // Show & hide new idea container
+    $("#new-entry ").hide();
+    $(".showbtn").show();
+    
+    $('.showbtn').click(function(){
+    $("#new-entry ").slideToggle();
+    });
+        
+}
+});
